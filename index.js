91e229935c82d80790a0edcd41942b9d61c20bb4
ricardoYecla <ricardoyecla@gmail.com>
2022-03-15 19:32:49 +0100

    primer post

--- index.js
+++ index.js
@@ -7,6 +7,10 @@ const cors = require('cors')
 //importamos mongoose
 const mongoose = require('mongoose');
 
+//Improtar modelos
+
+const Product = require('./models/product')
+
 const app=express()
 
 app.use(bodyParser.urlencoded({extended:false}))
@@ -17,15 +21,30 @@ app.use(cors())
     res.send({message:`Hola ${req.params.name}`})
 }) */
 app.get('/api/product',(req,res)=>{
-    console.log(req.body)
+    //console.log(req.body)
     res.status(200).send({"id":122,"nombre":"otro"})
 })
+
 app.get('/api/product/:productId',(req,res)=>{
     
 })
 app.post('/api/product',(req,res)=>{
+    console.log('POST /api/product')
     console.log(req.body)
-    res.status(200).send({message:`Producto recibido`})
+    
+    let product= new Product();
+    product.name= req.body.name;
+    product.picture=req.body.picture;
+    product.price=req.body.price;
+    product.category=req.body.category;
+    product.description=req.body.description;
+
+    product.save((err,productoStrored)=>{
+        if (err) res.status(500).send({message:`Error al salvar en la base de datos: ${err}`})
+
+        res.status(200).send({product: productoStrored})
+    })
+    
 })
 app.put('/api/product/:productId',(req,res)=>{
     
@@ -34,15 +53,29 @@ app.delete('/api/product/:productId',(req,res)=>{
     
 })
 
-mongoose.connection('MONGODB_URI=mongodb+srv://ricardoyecla:8xm3rryhysf4MKwc@cluster0.h6pyu.mongodb.net/shop?retryWrites=true&w=majority',(err,res)=>{
+/* mongoose.connection('MONGODB_URI=mongodb+srv://ricardoyecla:8xm3rryhysf4MKwc@cluster0.h6pyu.mongodb.net/shop?retryWrites=true&w=majority',(err,res)=>{
     if(err) throw err
     console.log('conexión a mongodb realizada.')
     
-    app.listen(port,()=>{
+    
+}) */
+
+const MONGODB_URI='mongodb+srv://ricardoyecla:8xm3rryhysf4MKwc@cluster0.h6pyu.mongodb.net/shop?retryWrites=true&w=majority'
+const port = process.env.PORT || 3000
+
+mongoose.connect(MONGODB_URI,(err,res)=>{
+        if (err) {
+            return console.log(`Error al conecta a la mongod Atlas: ${err}`)
+        }
+        console.log('Connected to MongoDB Atlas')
+        app.listen(port,()=>{
     console.log(`Mi acpi rest corrientdo en ${port}`)
 })
 })
-const port = process.env.PORT || 3000
+    
+
+
+
 
 
 
